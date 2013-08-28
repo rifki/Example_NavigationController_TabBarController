969e598afdeeb993947216dbcc82ad6c7e25688a
Muhamad Rifki <rifki@rifkilabs.net>
2013-08-29 01:08:09 +0700

    Update HomeViewController.m

--- NavigationAndTabbar/HomeViewController.m
+++ NavigationAndTabbar/HomeViewController.m
@@ -26,29 +26,29 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    self.view.backgroundColor = [UIColor greenColor];
     
-    UITableView *HometableView = [[UITableView alloc] initWithFrame:CGRectMake(0.0f, 0.0f, 320.0f, 470.0f) style:UITableViewStylePlain];
+    /*UITableView *HometableView = [[UITableView alloc] initWithFrame:CGRectMake(0.0f, 0.0f, 320.0f, 470.0f) style:UITableViewStylePlain];
     HometableView.dataSource = self;
     
-	// Do any additional setup after loading the view.
-    self.view.backgroundColor = [UIColor greenColor];
-    /*UIActionSheet *actionsheet = [[UIActionSheet alloc] initWithTitle:@"Action Title"
+    UIActionSheet *actionsheet = [[UIActionSheet alloc] initWithTitle:@"Action Title"
                                                              delegate:nil
                                                     cancelButtonTitle:@"Cancel" destructiveButtonTitle:nil otherButtonTitles:@"One",@"Two",@"Three",@"Four",@"Five",@"One",@"Two",@"Three",@"Four",@"Five",@"One",@"Two",@"Three",@"Four",@"Five",
                                   nil];
-    [actionsheet showInView:self.view];*/
+    [actionsheet showInView:self.view];
 
-    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(10.0f, 20.0f, 100.0f, 50.0f)];
+    /*UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(10.0f, 20.0f, 100.0f, 50.0f)];
     [label setText:@"My Home"];
     //UIColor *color = [UIColor co]
     [label setBackgroundColor:[UIColor blueColor]];
 
     [self.view addSubview:HometableView];
-    //[HometableView addSubview: label];
+    [HometableView addSubview: label];*/
     
 }
 
-
+/*
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     
@@ -64,14 +64,14 @@
    // NSDate *object = _objects[indexPath.row];
     cell.textLabel.text = @"object";
     return cell;
-    
-}
+}*/
 
+/*
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
     return 10;
 }
-
+*/
 
 - (void)didReceiveMemoryWarning
 {
